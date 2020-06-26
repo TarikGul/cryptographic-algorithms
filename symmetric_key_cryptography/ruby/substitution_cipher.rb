85f141a8394e24deac5fb770a417e5c492faef97
Tarik <tariksnow37@gmail.com>
2020-06-25 21:31:23 -0400

    substitution cipher

--- symmetric_key_cryptography/ruby/substitution_cipher.rb
+++ symmetric_key_cryptography/ruby/substitution_cipher.rb
@@ -24,4 +24,23 @@ def create_object_key
   key
 end
 
-create_object_key()
+def substitution_cipher (str, key)
+  new_str = [];
+
+  str.each_char do |char|
+    new_str << key[char]
+  end
+
+  encrypted_str = new_str.join('')
+  puts encrypted_str
+  encrypted_str
+end
+
+key1 = create_object_key()
+substitution_cipher('abc', key1)
+
+key2 = create_object_key()
+substitution_cipher('message', key2)
+
+key3 = create_object_key()
+substitution_cipher('substitutionmessage', key3)
