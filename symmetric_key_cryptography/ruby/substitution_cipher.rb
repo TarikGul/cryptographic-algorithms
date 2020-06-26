6eced19513ee815d2990e41b1219201761b10149
Tarik <tariksnow37@gmail.com>
2020-06-25 20:25:56 -0400

    write a unique hash algorithm different from the JS version for 2 different references

--- symmetric_key_cryptography/ruby/substitution_cipher.rb
+++ symmetric_key_cryptography/ruby/substitution_cipher.rb
@@ -1,5 +1,27 @@
-
+require('byebug');
 
 def create_object_key 
   key = {}
+
+  used_indicies = [];
+  alpha = 'abcdefghijklmnopqrstuvwxyz'
+
+  (0...alpha.length).each do |i|
+    random = 0
+
+    unique = false
+    while(!unique) do
+        random = rand(0...alpha.length)
+
+        unique = true if(!used_indicies.include?(random))
+    end
+
+    used_indicies << random
+    key[alpha.split('')[i]] = alpha[random];
+  end
+
+  puts key
+  key
 end
+
+create_object_key()
