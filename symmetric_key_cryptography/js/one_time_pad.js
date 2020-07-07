3a58dee7eb9cbb4c4d03867ea765b2f17554e300
Tarik <tariksnow37@gmail.com>
2020-07-07 14:40:21 -0400

    import ascii protocol

---
+++ symmetric_key_cryptography/js/one_time_pad.js
@@ -0,0 +1,7 @@
+// A common method for encoding letters is the (ASCII) protocol
+// I am going to write out the binary equivelent for all the letters so you understand
+// This comes from the ASCII
+
+// ASCII protocol
+const binaries = require('../../util/ASCII_protocol');
+
