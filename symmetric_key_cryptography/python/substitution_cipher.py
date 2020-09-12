345ba0a38a0993ad931eedf06e5ced91cc92d73a
Tarik <tariksnow37@gmail.com>
2020-09-12 15:35:47 -0700

    python substitution cipher

---
+++ symmetric_key_cryptography/python/substitution_cipher.py
@@ -0,0 +1,44 @@
+from random import seed
+from random import randrange
+
+def create_object_key():
+    key = {}
+
+    used_indices = []
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+
+    for i in range(len(alpha)):
+        random_num = 0
+        unique = False
+
+        while not unique:
+            random_num = randrange(len(alpha))
+
+            if random_num not in used_indices:
+                unique = True
+        
+        # check below lines for syntax
+        used_indices.append(random_num)
+        key[alpha[i]] = alpha[random_num]
+
+    print(key)
+    return key
+
+key = create_object_key()
+
+def substitution_cipher(str, key):
+    new_str = []
+
+    for char in str:
+        new_str.append(key[char])
+    
+    encrypted_str = ''.join(new_str)
+
+    print(encrypted_str)
+    encrypted_str
+
+
+substitution_cipher('hello', key)
+
+            
+
